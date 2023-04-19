c4df7a724917c03ab18506d07dfe808c7d74b035
Ga0Kwon <kgayeong30@gmail.com>
2023-04-19 16:49:31 +0900

    DAY75 할일 추가 event(엔터, 입력, 버튼 클릭)

--- src/main/frontend/src/example/exBook/AddTodo.js
+++ src/main/frontend/src/example/exBook/AddTodo.js
@@ -8,14 +8,51 @@ export default function AddTodo(props) {
     //사용자가 입력한 데이터를 저장할 상태변수
     const [item, setItem] = useState({title : ""}) //초기값 깡통
 
+    //1. 사용자가 입력한 데이터를 가져오기
+    //onChange={onInputChange}
+    //onChange 이벤트 속성 : 헤당 요소가 해당 이벤트를 발생했을 때 이벤트 정보를 반환[e]
+    //onChange 이벤트 정보 e를 onInputChange 함수로 매개변수 e로 전달
+
+   const onInputChange =  (e) => {
+        //console.log(e) : 해당 이벤트가 발생했을 때의 이벤트 정보 들어있는 객체
+        //console.log(e.target); : 해당 이벤트가 발생된 요소 [TextField]
+        setItem({title : e.target.value}) //: 상태변경 set~~~ : 입력받은 값을 가져와서 상태변수를 수정
+        //console.log(item);
+    }
+
+   //2. AppTodo로 부터 전달받은 addItem 함수
+    const addItem = props.addItem;
+   //3. + 버튼을 클릭했을 때
+    const onButtonClick =  (e) => {
+            addItem(item);
+            //입력받은 데이터를 AppTodo컴포넌트 한테 전달받은 addItem함수의 매개변수 넣고 함수 실행
+            setItem({title : ""});
+    }
+
+    //4. 엔터를 입력했을 때
+    const enterKeyEventHandler = (e) =>{
+        if(e.key === 'Enter' || e.keyCode == 32){
+            onButtonClick();
+        }
+    }
+
     return(<>
         {/*style {}안에 지정할때 하이픈(-)안쓰고 카멜표기법을 사용*/}
         <Grid container style = {{marginTop : 20}}>
             <Grid xs ={11} md = {11} item style = {{paddingRight:16}}>
-                <TextField placeholder ="AddTodo(할일 작성)" fullWidth/>
+                <TextField
+                placeholder ="AddTodo(할일 작성)"
+                fullWidth
+                value = {item.title} /*상태변수 초기화*/
+                onChange={onInputChange}
+                onKeyPress={enterKeyEventHandler}/>
             </Grid>
             <Grid xs = {1} md = {1} item>
-                <Button fullWidth style = {{heigth : '100%'}} color = "secondary" variant = "outlined">
+                <Button
+                fullWidth style = {{height : '100%'}}
+                color = "secondary"
+                variant = "outlined"
+                onClick = {onButtonClick}>
                     +
                 </Button>
             </Grid>
--- src/main/frontend/src/example/exBook/AppTodo.js
+++ src/main/frontend/src/example/exBook/AppTodo.js
@@ -13,27 +13,25 @@ export default function AppTodo(props) {
     //item 객체에 { id : "0", title : "Hello world 1",  done : true}
     const[items, setItems] = useState(
        [
-           {
-                id : "0",
-                title : "Hello world 1",
-                done : true
-            },
-
-             {
-                id : "1",
-                title : "Hello world 2",
-                done : false
-             }
 
        ] //array
     )
+
+    //2. items에 새로운 item을 등록하는 함수
+    const addItem = (item) => { //함수로부터 매개변수로 전달받은 item
+        item.id = "ID-" + item.length //ID 구성
+        item.done = false; //체크 여부
+        setItems([...items, item]); // 기존 상태 items 메 item 추가
+        //setItems([...상태명, 값]); : ... 앞에 추가 : react의 규칙
+    }
+
     //반복문 이용한 Todo 컴포넌트 생성
     let TodoItems =
         /*<Paper style = "maign:16px;"> : HTML 방식*/
-        <Paper style = {{margin:16}}> {/*JSX의 style 속성 방법 중괄호 두번*/}
+        <Paper style = {{margin:16}}>  {/*JSX의 style 속성 방법 중괄호 두번*/}
             <List>{
                 items.map((i) =>
-                   <Todo item ={i} />
+                   <Todo item ={i} key = {i.id} />
                  )
                }
            </List>
@@ -43,7 +41,7 @@ export default function AppTodo(props) {
         <div clasName="App">
             {/* Container : 박스권 화면 가운데로 잡아주는 컴포넌트*/}
             <Container maxWidth ="md">
-                <AddTodo />
+                <AddTodo addItem ={addItem}/> {/*함수를 전달*/}
                 {/*data 와 같은 props를 전달할때는 이름 아무거나 해도 된다.*/}
                 {TodoItems}
             </Container>
