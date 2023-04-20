4cf06273be5a16f3f79c0c93134d0536c2cc362c
Ga0Kwon <Kgayeong39@gmail.com>
2023-04-20 11:27:37 +0900

    DAY76 mui icon 설치 및 todo 삭제

--- src/main/frontend/src/example/exBook/AppTodo.js
+++ src/main/frontend/src/example/exBook/AppTodo.js
@@ -19,10 +19,35 @@ export default function AppTodo(props) {
 
     //2. items에 새로운 item을 등록하는 함수
     const addItem = (item) => { //함수로부터 매개변수로 전달받은 item
-        item.id = "ID-" + item.length //ID 구성
+        item.id = "ID-" + items.length //ID 구성 [items.length 식으로 하면 문제가 있음]
         item.done = false; //체크 여부
         setItems([...items, item]); // 기존 상태 items 메 item 추가
+        //item = {title : "입력받은값", id = "ID-배열길이", done : "기본값 false"}
         //setItems([...상태명, 값]); : ... 앞에 추가 : react의 규칙
+    }
+
+    //3. item에 삭제할 item 삭제
+    const deleteItem = (item) =>{
+        console.log(item.id);
+        // 만약에 items에 있는 item중 삭제할 id와 다른경우(삭제한 item이 아닌거니까) 해당 item 반환
+        const newItems = items.filter(i => i.id !== item.id);
+            // * 삭제할 id를 제외한 새로운 newItems 배열이 선언
+        setItems([...newItems])
+
+        //JS 반복문 함수 제공
+            //r = [1, 2, 3]
+            //배열/리스트.forEach((o) => {}) : 반복문만 가능 [return 없음]
+                //let array = r.forEach((o) => { o+3 });
+                // 반복문이 끝나도 array에 아무것도 들어있지 않다.
+
+            //배열/리스트.map((o) => { }) : + return 값들을 새로운 배열 저장
+                // let array = r.map((o) =? {return o + 3});
+                // 반복문이 끝나면 array에는 [4, 5, 6]
+
+            //배열/리스트.filter((o) => { }) : + 조건 충족할 경우 객체 반환
+                // let array = r.filter((o) => { o>= 3});
+                //반복문이 끝나면 array에는 [3]
+
     }
 
     //반복문 이용한 Todo 컴포넌트 생성
@@ -31,7 +56,7 @@ export default function AppTodo(props) {
         <Paper style = {{margin:16}}>  {/*JSX의 style 속성 방법 중괄호 두번*/}
             <List>{
                 items.map((i) =>
-                   <Todo item ={i} key = {i.id} />
+                   <Todo item ={i} key = {i.id} deleteItem = {deleteItem}/>
                  )
                }
            </List>
--- src/main/frontend/src/example/exBook/Todo.js
+++ src/main/frontend/src/example/exBook/Todo.js
@@ -1,15 +1,33 @@
 //컴포넌트 만들기
 import React, {useState} from 'react';
 
-import {ListItem, ListItemText, InputBase, Checkbox} from '@mui/material';
+import {ListItem, ListItemText, InputBase, Checkbox,
+        ListItemSecondaryAction, IconButton} from '@mui/material';
+
+//IconButton : icon이 들어가는 박스권[hover도 있는듯]
+//아이콘 설치[터미널] : npm install @mui/icons-material [ 다양한 아이콘 제공 ]
+
+//삭제 아이콘
+import DeleteOutlined from '@mui/icons-material/DeleteOutlined';
 
 //react Bootstrap 버전이 따로 있다.
 //=> mui 를 더 많이 쓴다.
+
+
+
 
 export default function Todo(props) {
 
     //1. Hook 상태관리 useState
     const [items, setItems] = useState(props.item);
+
+    //2. props로 전달된 삭제함수 변수로 이동
+    const deleteItem = props.deleteItem;
+
+    //3. 삭제함수 이벤트처리 핸들러
+    const deleteEventHandler = () => {
+        deleteItem(items);
+    }
 
     return (<>
         <ListItem>
@@ -24,6 +42,12 @@ export default function Todo(props) {
                     fullWidth={true}
                 />
             </ListItemText>
+
+            <ListItemSecondaryAction>
+                <IconButton onClick = {deleteEventHandler}>
+                    <DeleteOutlined/>
+                </IconButton>
+            </ListItemSecondaryAction>
         </ListItem>
     </>)
 }
