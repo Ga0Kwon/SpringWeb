3198c28b0174719257d8b484133c948493ca5ca1
Ga0Kwon <kgayeong39@gmail.com>
2023-05-03 14:03:40 +0900

    DAY85 웹소켓 메시지 형식 변경, 익명 난수 생성, JSON 전송

--- src/main/frontend/src/component/board/Chatting.js
+++ src/main/frontend/src/component/board/Chatting.js
@@ -4,6 +4,8 @@ import {Container} from '@mui/material'
 
 export default function Chatting(props) { //export : 내보내기
 
+    let[id, setId] = useState(''); //익명채팅에서 사용할 id [난수 저장]
+
     //채팅 입력창[input] DOM 객체 제어 변수
     let msgInput = useRef(null);
 
@@ -13,36 +15,52 @@ export default function Chatting(props) { //export : 내보내기
    //let webSocket = new WebSocket("ws://localhost:8080/서버주소");
 
    //2. 재렌더링 될 때 데이터 상태 유지
-   let webSocket = useRef(new WebSocket("ws://localhost:8080/chat"));
+   let webSocket = useRef(null);
 
-   //3. 클라이언트 소켓이 서버 소켓에 접속했을 때 이벤트
-   webSocket.current.onopen = ()=>{console.log("웹 소켓 접속했습니다.");}
+    useEffect (() => {
+        if(!webSocket.current){//만약 클라이언트 소켓이 없을 때(접속이 안되어 있을 때.
+               webSocket = useRef(new WebSocket("ws://localhost:8080/chat"));
 
-   //4. 서버소켓 나갔을 때
-   webSocket.current.onclose = (e)=>{console.log("웹 소켓 나갑니다.");}
+               //3. 클라이언트 소켓이 서버 소켓에 접속했을 때 이벤트
+               webSocket.current.onopen = ()=>{
+                    //floor : 소숫점 자르는 거
+                    let randomId = Math.floor(Math.random() * (9999-1) + 1); // 1~9999
+                    setId("익명"+randomId);
+                    console.log("웹 소켓 접속했습니다.");
+               }
 
-   //5. 서버소켓과 오류가 발생했을 때
-   webSocket.current.onerror = (e)=>{console.log("웹 소켓 에러 발생했습니다.");}
+               //4. 서버소켓 나갔을 때
+               webSocket.current.onclose = (e)=>{console.log("웹 소켓 나갑니다.");}
 
-   //6. 서버소켓으로부터 메시지 받았을 때
-   webSocket.current.onmessage =  (e)=>{
-        console.log("웹 소켓 메시지왔습니다.");
-        console.log(e.data);
+               //5. 서버소켓과 오류가 발생했을 때
+               webSocket.current.onerror = (e)=>{console.log("웹 소켓 에러 발생했습니다.");}
 
-        /*msgContent.push(e.data); //배열에 내용 추가
-        setMsgContent([...msgContent]); //재 렌더링 기존 배열을 새롭게 하는 것*/
+               //6. 서버소켓으로부터 메시지 받았을 때
+               webSocket.current.onmessage =  (e)=>{
+                    console.log("웹 소켓 메시지왔습니다.");
+                    console.log(e.data);
 
-        setMsgContent([...msgContent, e.data]); //재 렌더링, 기존 배열에 e.data를 넣어주는
-   }
+                    /*msgContent.push(e.data); //배열에 내용 추가
+                    setMsgContent([...msgContent]); //재 렌더링 기존 배열을 새롭게 하는 것*/
+                    let data = JSON.parse(e.data); //형변환 JSON 형식으로
+                    setMsgContent((msgContent) => [...msgContent, data]); //재 렌더링, 기존 배열에 e.data를 넣어주는
+               }
+        }
+    })
 
 
+    //7. 메시지 전송
     const onSend = ()=>{
 /*        console.log(msgInput.current.value);
         console.log(document.querySelector(".inputBox").value)*/
 
-        let msg = msgInput.current.value;
+        let msgBox = {
+            id : id,
+            msg : msgInput.current.value,
+            time : new Date().toLocalTimeString() //현재 시간만 빼옴
+        }
         //msgInput 변수가 참조중인  <input ref = {msgInput} type ="text" className="inputBox"/>해당 input을 DOM객체로 호출
-        webSocket.current.send(msg); //클라이언트가 서버에게 메시지를 전송 [.send()]
+        webSocket.current.send(JSON.stringify(msgBox)); //클라이언트가 서버에게 메시지를 전송 [.send()]
 
         msgInput.current.value = "";
     }
@@ -63,7 +81,7 @@ export default function Chatting(props) { //export : 내보내기
                 }
             </div>
             <div className="chatInputBox">
-                <span>익명00</span>
+                <span>{id}</span>
                 <input ref = {msgInput} type ="text" className="inputBox"/>
                 <button onClick={onSend}>전송</button>
             </div>
@@ -95,7 +113,7 @@ const[id, setId] = useState(0); //set 메소드 사용시 컴포넌트 재호출
 
     //2. 난수 생성
     let randId = Math.floor(Math.random() * (9999 -1) + 1);
-    // Math.floor(Math.random() * (최댓값 -최솟값) + 최솟값) : 정수 1부터 1000까지
+    // Math.floor(Math.random() * (최댓값 -최솟값) + 최솟값) : 정수 1부터 9999까지
 
     숫자 = randId;
     setId(randId);
